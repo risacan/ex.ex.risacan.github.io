e82ff4284aa764ddcc9028d4811885195430c80a
Risa Harada <supikateriburu@gmail.com>
2019-02-11 16:25:26 +0900

    :wrench: Fix path

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ for (var i = 0, l = elms.length; l > i; i++) {
 	hljs.highlightBlock(elms[i]);
 }
 
-if (window.location.href === "https://risacan.net") {
+if (window.location.href === "https://risacan.net/") {
 	window.location = "https://www.notion.so/risacan/risacan-043429a61bd84c86aa17756b58e0d9f2";
 }

558e4c85c0a71e3f7a8bbf551dc1af08b0bcd774
Risa Harada <supikateriburu@gmail.com>
2019-02-11 16:23:15 +0900

    :wheel_of_dharma:

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 var elms = document.getElementsByTagName("figure");
 
-for( var i=0 , l=elms.length ; l>i ; i++ ) {
-	hljs.highlightBlock( elms[i] ) ;
+for (var i = 0, l = elms.length; l > i; i++) {
+	hljs.highlightBlock(elms[i]);
+}
+
+if (window.location.href === "https://risacan.net") {
+	window.location = "https://www.notion.so/risacan/risacan-043429a61bd84c86aa17756b58e0d9f2";
 }
